8083984cc0e96aaef6db11f07cd2821f8b4a6063
Mary Ashton Inglis <maryashton.inglis@gmail.com>
2013-09-17 22:39:07 -0400

    This is the corrected index page.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Mary Ashton's P1 Page</title>
+	<link rel="stylesheet" href="main.css" type="text/css">
+
+</head>
+
+<body>
+	<h1>Short Bio</h1>
+	<p>Hi there, I'm "Mary Ashton" and I do go by both names.  It's a Southern thing. I'm from a tiny town called Travelers Rest in northwest South Carolina.</p>
+	<p>I grew up on a small farm, studied music in college, and am now working as a non-profit development director. I am thrilled to be taking this class and learning more about how to make awesome web applications!</p>
+	<h1>Text Editor</h1>
+	<p>I am using Sublime Text 2 as a text editor for this course.</p>
+</body>
+
+</html>
